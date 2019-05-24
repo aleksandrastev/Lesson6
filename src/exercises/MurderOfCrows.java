a37a778df57860d402684e95aaf013bc1462d781
aleksandrastev <leafsoul91@gmail.com>
2019-05-24 20:29:12 +0200

    Added method for finding diamond

--- src/exercises/MurderOfCrows.java
+++ src/exercises/MurderOfCrows.java
@@ -1,5 +1,7 @@
 package exercises;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MurderOfCrows {
@@ -13,13 +15,26 @@ public class MurderOfCrows {
 	}
 
 	private void findTheDiamond() {
+		int counter = 0;
 		/*
-		 * 1. One of the Crows has eaten the diamond. You need to search through the stomach of each Crow, 
-		 * then print the name of the guilty Crow.
+		 * 1. One of the Crows has eaten the diamond. You need to search through the
+		 * stomach of each Crow, then print the name of the guilty Crow.
 		 */
-		
+		for (int i = 0; i < theMurder.size(); i++) {
+			String stomachContents = Arrays.toString(theMurder.get(i).getStomachContents().toArray());
+			System.out.println(
+					"Oops.... I killed the wrong Crow! I found the " + stomachContents.replace("[", "").replace("]", "")
+							+ " in the " + theMurder.get(i).getName() + "'s stomach.");
+			if (theMurder.get(i).getStomachContents().contains("diamond")) {
+				System.out.println("Name of the guilty Crow is " + theMurder.get(i).getName() + ".");
+				break;
+			} else {
+				counter++;
+			}
+		}
 		/* 2. How many innocent crows had to die before the diamond was found? */
-		
+		System.out.println(counter + " innocent crows had to die before the diamond was found!");
+
 	}
 
 	private void initializeCrows() {
@@ -53,11 +68,16 @@ class Crow {
 
 	private String getRandomCrowFood() {
 		int randomness = new Random().nextInt(4);
-		if (randomness == 0) return "carrion";
-		else if (randomness == 1)return "snail";
-		else if (randomness == 2)return "acorn";
-		else if (randomness == 3)return "spider";
-		else return "grub";
+		if (randomness == 0)
+			return "carrion";
+		else if (randomness == 1)
+			return "snail";
+		else if (randomness == 2)
+			return "acorn";
+		else if (randomness == 3)
+			return "spider";
+		else
+			return "grub";
 	}
 
 	public String getName() {
@@ -69,5 +89,3 @@ class Crow {
 	}
 
 }
-
-
