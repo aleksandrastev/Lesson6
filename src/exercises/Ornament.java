698b829de0f3302e81b2afdc09e84c371c78e698
aleksandrastev <leafsoul91@gmail.com>
2019-05-24 20:26:36 +0200

    Added color and constructor to the Ornament class

--- src/exercises/Ornament.java
+++ src/exercises/Ornament.java
@@ -1,15 +1,25 @@
 package exercises;
 
+import java.util.Random;
+
 /* 
  * Create a class called Decorator that contains an ArrayList called boxOfDecorations.
  *   1. In the Decorator class, hang all the ornaments in the box.
- *   2. Add a color to the Ornament class so that your program prints “Hanging a red ornament”, etc.
+ *   2. Add a color to the Ornament class so that your program prints "Hanging a red ornament", etc.
  */
 
 public class Ornament {
-	
-	public void hang(){
-		System.out.println("hanging an ornament");
+	String color;
+
+	public Ornament() {
+		Random r = new Random();
+		String[] colors = { "black", "white", "red", "blue", "green", "yellow", "pink", "grey", "purple", "brown" };
+		this.color = colors[r.nextInt(colors.length)];
+	}
+
+	public void hang() {
+		System.out.println("Hanging a " + this.color + " ornament");
+
 	}
 
 }
