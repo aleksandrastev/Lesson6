3b1b26b7d4428aa69f534a7d81dce1bb6afee440
aleksandrastev <leafsoul91@gmail.com>
2019-05-24 20:30:15 +0200

    Code added

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -3,6 +3,7 @@ package exercises;
 import junit.framework.TestCase;
 
 public class StringReversal extends TestCase {
+	
 	/*
 	 * Without using the reverse() method in StringBuffer or StringBuilder, write a
 	 * string reverser that will make this unit test pass.
@@ -11,8 +12,19 @@ public class StringReversal extends TestCase {
 	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
-//	}
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+		System.out.println(reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+	}
+	private String reverseMe(String string) {
+		String reversedString = "";
+		char[] stringToChar = string.toCharArray();
+		for (int i = stringToChar.length-1; i >= 0; i--) {
+			reversedString += stringToChar[i];
+		}
+		return reversedString;
+	}
+	
+	
 
 }
