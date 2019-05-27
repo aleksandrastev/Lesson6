23d63ad115d02725b4f875b36c09c51e7191d5c3
aleksandrastev <leafsoul91@gmail.com>
2019-05-27 13:38:40 +0200

    Code added to PeaceOnEarth class

--- src/optional/PeaceOnEarth.java
+++ src/optional/PeaceOnEarth.java
@@ -1,6 +1,7 @@
 package optional;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class PeaceOnEarth {
 	public static void main(String[] args) {
@@ -21,7 +22,24 @@ public class PeaceOnEarth {
 		truth.add('k');
 
 		/* TODO: Remove the hash symbols and print out the truth. */
-		
-		
+		for (int i = 0; i < truth.size(); i++) {
+			if (truth.get(i).equals('#')) {
+				truth.remove(i);
+				i--;
+			}
+		}
+
+		// 2 ways to print chars as string
+
+		StringBuilder sb = new StringBuilder();
+		for (Character ch : truth) {
+			sb.append(ch);
+		}
+		String string = sb.toString();
+		System.out.println(string);
+
+		for (int i = 0; i < truth.size(); i++) {
+			System.out.print(truth.get(i).toString());
+		}
 	}
 }
